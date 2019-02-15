6e046cb7239f5bc6b01ea9a91ec1c577a06f168e
Nathan Griffith <nategri@gmail.com>
2019-02-14 21:01:36 -0800

    re-orgnaize project a la nanotode

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
 emcc --embed-file fonts \
   --embed-file img \
-  src/main.c -o index.js \
-  -I./include \
-  -I./nanotode/include \
-  nanotode/src/*.c \
-  src/worm.c \
-  src/behaviors.c \
-  src/muscle_display.c \
-  src/motion_component_display.c \
-  src/trap.c \
-  src/text_box.c \
+  source/main.c -o index.js \
+  -I./source \
+  -I./nanotode/source \
+  nanotode/source/*.c \
+  source/*.c \
   -s WASM=1 \
   -s USE_SDL=2 \
-  -s USE_SDL_TTF=2 \
+  -s USE_SDL_TTF=2
